43e056de2bc0c59070b4d1683c45dc93831f3baf
Luis Velandia <v-lvelandia@youngliving.com>
2024-01-06 21:59:46 -0500

    33 Estado del componente

--- 02-bases-vue/app.js
+++ 02-bases-vue/app.js
@@ -1,8 +1,14 @@
 const app = Vue.createApp({
-    template: `
-    <h1>Hola Mundo</h1>
-    <p>{{ 1 +1 }}</p>
-    `
+    // template: `
+    // <h1>Hola Mundo</h1>
+    // <p>{{ 1 +1 }}</p>
+    // `
+    data() {
+        return {
+            quote: `I'm batman`,
+            author: '-Batman'
+        }
+    }
 })
 
 app.mount('#myApp')
