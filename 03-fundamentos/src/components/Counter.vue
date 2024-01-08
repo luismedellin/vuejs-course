8ef341e082890b6371cb16f6fea9672c89286320
Luis Velandia <v-lvelandia@youngliving.com>
2024-01-08 10:50:08 -0500

    51 Incrementar decrementar

--- 03-fundamentos/src/components/Counter.vue
+++ 03-fundamentos/src/components/Counter.vue
@@ -1,9 +1,10 @@
 <template>
   <h2>Counter</h2>
   <p>{{ counter }} <sup>2</sup> = {{ squareCounter }}</p>
-  <p>{{ counter }} <sup>2</sup> = {{ squareCounter }}</p>
-  <p>{{ counter }} <sup>2</sup> = {{ squareCounter }}</p>
-  <p>{{ counter }} <sup>2</sup> = {{ squareCounter }}</p>
+  <button @click="decrease">-1</button>
+  <button @click="update(-1)">-1</button>
+  <button @click="increase">+1</button>
+  <button @click="update(+1)">+1</button>
 </template>
 
 <script>
@@ -16,6 +17,15 @@ export default {
   methods: {
     getSqureValue() {
       return this.counter * this.counter
+    },
+    increase() {
+      this.counter++;
+    },
+    decrease() {
+      this.counter--;
+    },
+    update(value) {
+      this.counter += value
     }
   },
   computed: {
@@ -27,5 +37,21 @@ export default {
 </script>
 
 <style>
+
+button {
+  background-color: #64B6B7;
+  border-radius: 5px;
+  border: 1px solid white;
+  color: white;
+  cursor: pointer;
+  margin: 0 5px;
+  padding: 5px 15px;
+  transition: 0.3s ease-in-out;
+}
+
+button: hover {
+  background-color: #5aa67b;
+  transition: 0.3s ease-in-out;
+}
 
 </style>
