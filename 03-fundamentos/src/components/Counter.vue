71f987438c146628fcfcaa161cbda40a948319b2
Luis Velandia <v-lvelandia@youngliving.com>
2024-01-08 10:32:56 -0500

    49 Estados del componente

--- 03-fundamentos/src/components/Counter.vue
+++ 03-fundamentos/src/components/Counter.vue
@@ -1,11 +1,15 @@
 <template>
   <h2>Counter</h2>
-  <p>10</p>
+  <p>{{ counter }} <sup>2</sup> = {{ counter*counter }}</p>
 </template>
 
 <script>
 export default {
-  name: 'Patito'
+  data() {
+    return {
+      counter: 5
+    }
+  }
 }
 </script>
 
