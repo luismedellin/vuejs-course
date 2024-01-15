b59829b684539e7deaf32e97a46725e594645e42
Luis Velandia <luiseduardo1218@gmail.com>
2024-01-15 16:00:10 -0500

    90. Diseño de los componentes

--- 04-pokemon-game/src/components/PokemonPicture.vue
+++ 04-pokemon-game/src/components/PokemonPicture.vue
@@ -1,5 +1,15 @@
 <template>
-  <h3>Pokemon Picture</h3>
+    <div class="pokemon-container">
+          <img
+            class="hidden-pokemon"
+            src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg"
+            alt="pokemon">
+        <img
+            class="fade-in"
+            src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg"
+            alt="pokemon">
+    </div>
+
 </template>
 
 <script>
@@ -8,6 +18,21 @@ export default {
 }
 </script>
 
-<style>
-
+<style scoped>
+    .pokemon-container {
+        height: 200px;
+    }
+    img {
+        height: 200px;
+        position: absolute;
+        right: 32%;
+        user-select: none;
+        -moz-user-select: none;
+        -ms-user-select: none;
+        -webkit-user-drag: none;
+        -webkit-user-select: none;
+    }
+    .hidden-pokemon {
+        filter: brightness(0);
+    }
 </style>
