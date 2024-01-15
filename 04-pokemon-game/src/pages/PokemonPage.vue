b33e497019dc3ad3b785ab63ea37155b080f84a7
Luis Velandia <luiseduardo1218@gmail.com>
2024-01-15 15:49:54 -0500

    89. Estructura del proyecto y componentes

---
+++ 04-pokemon-game/src/pages/PokemonPage.vue
@@ -0,0 +1,15 @@
+<template>
+    <h1>¿Quién es este Pokémon?</h1>
+
+    <PokemonPicture />
+    <PokemonOption />
+</template>
+
+<script>
+import PokemonOption from '@/components/PokemonOption.vue'
+import PokemonPicture from '@/components/PokemonPicture.vue'
+
+export default {
+  components: { PokemonPicture, PokemonOption },
+}
+</script>
