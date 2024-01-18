35346085e37a96f327c8a1183f098b242664bc0d
Luis Velandia <luiseduardo1218@gmail.com>
2024-01-17 22:32:31 -0500

    126. Disparar métodos del ciclo de vida

--- 05-rutas-ciclo/src/modules/pokemon/pages/AboutPage.vue
+++ 05-rutas-ciclo/src/modules/pokemon/pages/AboutPage.vue
@@ -1,3 +1,63 @@
 <template>
   <h1>About Page</h1>
+  <h2>{{name}}</h2>
+  <hr/>
+  <button @click="changeName">
+    Change Name
+  </button>
+
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      name: 'Luis'
+    }
+  },
+  methods: {
+    changeName() {
+      this.name = 'Luis Eduardo'
+    }
+  },
+  beforeCreate() {
+    console.log('beforeCreate')
+  },
+  created() {
+    console.log('created')
+  },
+  beforeMount() {
+    console.log('beforeMount')
+  },
+  mounted() {
+    console.log('mounted')
+  },
+  beforeUpdate() {
+    console.log('beforeUpdate')
+  },
+  updated() {
+    console.log('updated')
+  },
+  activated() {
+    console.log('activated')
+  },
+  deactivated() {
+    console.log('deactivated')
+  },
+  beforeUnmount() {
+    console.log('beforeUnmount')
+  },
+  unmounted() {
+    console.log('unmounted')
+  },
+  errorCapture() {
+    console.log('errorCapture')
+  },
+  renderTracked() {
+    console.log('renderTracked')
+  },
+  renderTriggered() {
+    console.log('renderTriggered')
+  },
+}
+</script>
