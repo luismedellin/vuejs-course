8373a10ddbf408e35ca296f793633c34e5a7ad17
Luis Velandia <luiseduardo1218@gmail.com>
2024-01-18 14:27:52 -0500

    129. Petición HTTP y redirecciones

--- 05-rutas-ciclo/src/modules/pokemon/pages/PokemonPage.vue
+++ 05-rutas-ciclo/src/modules/pokemon/pages/PokemonPage.vue
@@ -1,5 +1,8 @@
 <template>
     <h1>Pokemon Page <span>{{$route.params.id}}</span></h1>
+    <div v-if="pokemon">
+        <img :src="pokemon.sprites.front_default" :alt="pokemon.name" />
+    </div>
 </template>
 
 <script>
@@ -13,6 +16,7 @@ export default {
     data() {
         return {
             // id: this.$route.params.id
+            pokemon: null
         }
     },
 
@@ -21,7 +25,26 @@ export default {
         // const { id } = this.$route.params
         // console.log(id)
         // this.id = id
+        this.getPokemon()
         console.log(this.$attrs)
+    },
+    methods: {
+        async getPokemon() {
+            try{
+                const pokemon = await fetch(`https://pokeapi.co/api/v2/pokemon/${this.id}`)
+                    .then(r => r.json())
+                console.log(pokemon)
+                this.pokemon = pokemon
+            }catch (error) {
+                this.$router.push('/about')
+                console.log('No hay nada que hacer aquí')
+            }
+        }
+    },
+    watch: {
+        id() {
+            this.getPokemon()
+        }
     }
 }
 </script>
