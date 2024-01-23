5d2b97d2b74ab8d93d2d1f6597247edd0a0b40ff
Luis Velandia <luiseduardo1218@gmail.com>
2024-01-22 22:53:23 -0500

    131. RouterLink Personalizado

--- 05-rutas-ciclo/src/modules/shared/components/Navbar.vue
+++ 05-rutas-ciclo/src/modules/shared/components/Navbar.vue
@@ -1,10 +1,39 @@
+import { defineAsyncComponent } from 'vue';
 <template>
     <div>
-        <router-link to{name: 'home'}>Pokemon List</router-link>
+        <!-- <router-link to{name: 'home'}>Pokemon List</router-link>
         <router-link :to="{ name: 'pokemon-id', params: {id: 85}}">Pokemon por id</router-link>
-        <router-link to{name: 'about'}>About</router-link>
+        <router-link to{name: 'about'}>About</router-link> -->
+        <CustomLink
+            v-for="link in links"
+            :key="link.to"
+            :link="link"
+        />
     </div>
 </template>
+
+<script>
+
+import { defineAsyncComponent } from 'vue'
+
+export default {
+    data() {
+        return {
+            links: [
+                { to: '/home', name: 'Pokemons' },
+                { to: '/pokemonid/50', name: 'Por ID' },
+                { to: '/about', name: 'About' },
+                { to: 'https://google.com', name: 'Google' },
+            ]
+        }
+    },
+
+    components: {
+        CustomLink: defineAsyncComponent(() => import('./CustomLink.vue'))
+    }
+}
+
+</script>
 
 <style scoped>
 div {
