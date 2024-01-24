22295f68c8a75cd392d36d05c416b948dc911cdf
Luis Velandia <luiseduardo1218@gmail.com>
2024-01-24 09:07:18 -0500

    144. Leer el state reactivo

---
+++ 06-bases-vuex/src/components/Counter.vue
@@ -0,0 +1,26 @@
+<template>
+    <h1>Counter vuex</h1>
+    <h2>Direct access: {{ $store.state.count }}</h2>
+    <!-- <h2>Computed: {{ countComputed }}</h2> -->
+    <h1>mapState</h1>
+    <h2>mapState: {{ count }}</h2>
+    <h2>mapState: {{ lastMutation }}</h2>
+</template>
+
+<script>
+import { mapState } from 'vuex'
+
+export default {
+    // computed: mapState(['count'])
+    computed: {
+        countComputed() {
+            return this.$store.state.count
+        },
+        ...mapState(['count', 'lastMutation'])
+        // ...mapState({
+        //     count: state => state.count,
+        //     lastMutation: state => state.lastMutation
+        // })
+    }
+}
+</script>
