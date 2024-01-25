d043078e27abe5555ad6b31e5035905e34fb4ec6
Luis Velandia <luiseduardo1218@gmail.com>
2024-01-24 22:55:07 -0500

    150. Modules

--- 06-bases-vuex/src/store/index.js
+++ 06-bases-vuex/src/store/index.js
@@ -1,43 +1,10 @@
 import { createStore } from 'vuex'
+import counterStore from './counter'
 
-import getRandomInt from '../helpers/getRandomInt'
+
 
 export default createStore({
-    state: {
-        count: 1,
-        lastMutation: 'none',
-        isLoading: false,
-        lastRandomInt: 0
-    },
-
-    mutations: {
-        increment(state) {
-            state.count ++
-            state.lastMutation = 'increment'
-        },
-        incrementBy(state, value) {
-            state.count += value
-            state.lastMutation = 'incrementBy ' + value
-            state.lastRandomInt = value
-        },
-        setLoading(state, loading){
-            state.isLoading = loading
-            state.lastMutation = 'setLoading'
-        }
-    },
-
-    actions: {
-        async incrementRandomInt({commit}) {
-            commit('setLoading', true)
-            const randomInt = await getRandomInt()
-            commit('incrementBy', randomInt)
-            commit('setLoading', false)
-        }
-    },
-
-    getters: {
-        squareCount(state){
-            return state.count * state.count
-        }
+    module: {
+        counter: counterStore
     }
 })
