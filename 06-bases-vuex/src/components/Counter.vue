499a7bd5013ff536b25a1df93db12c01555e4c88
Luis Velandia <luiseduardo1218@gmail.com>
2024-01-24 22:13:05 -0500

    145. Mutations

--- 06-bases-vuex/src/components/Counter.vue
+++ 06-bases-vuex/src/components/Counter.vue
@@ -1,7 +1,12 @@
 <template>
     <h1>Counter vuex</h1>
     <h2>Direct access: {{ $store.state.count }}</h2>
-    <!-- <h2>Computed: {{ countComputed }}</h2> -->
+    <h2>Computed: {{ countComputed }}</h2>
+
+    <button @click="increment">+1</button>
+    <button @click="incrementBy(5)">+5</button>
+    <button>Random</button>
+
     <h1>mapState</h1>
     <h2>mapState: {{ count }}</h2>
     <h2>mapState: {{ lastMutation }}</h2>
@@ -21,6 +26,14 @@ export default {
         //     count: state => state.count,
         //     lastMutation: state => state.lastMutation
         // })
+    },
+    methods: {
+        increment() {
+            this.$store.commit('increment')
+        },
+        incrementBy(value) {
+            this.$store.commit('incrementBy', value)
+        }
     }
 }
 </script>
--- 06-bases-vuex/src/store/index.js
+++ 06-bases-vuex/src/store/index.js
@@ -4,5 +4,16 @@ export default createStore({
     state: {
         count: 1,
         lastMutation: 'none'
+    },
+
+    mutations: {
+        increment(state) {
+            state.count ++
+            state.lastMutation = 'increment'
+        },
+        incrementBy(state, value) {
+            state.count += value
+            state.lastMutation = 'incrementBy'
+        }
     }
 })
